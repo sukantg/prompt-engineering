8a6cc561f57ce4d239748bf6ac3435fbe6582b76
Sukant Ghosh <Sukant@Sukants-Air.home>
2023-06-25 11:07:33 +0100

    Prompt 1

---
+++ prompt1.py
@@ -0,0 +1,16 @@
+import openai
+
+openai.api_key = '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'
+
+def generate_text(prompt):
+    response = openai.Completion.create(
+        model='text-davinci-003',
+        prompt=prompt,
+        max_tokens=100
+    )
+    return response.choices[0].text.strip()
+
+
+prompt = 'The spiders in australia'
+result = generate_text(prompt)
+print(result)
